3ee36c428901873885808c12f12abbdbd002cd67
sushil2817 <tony281723@gmail.com>
2025-12-03 22:29:37 +0530

    style: format code in ApiError class for improved readability

--- src/utils/api-error.js
+++ src/utils/api-error.js
@@ -1,22 +1,23 @@
-class ApiError extends Error{
+class ApiError extends Error {
     constructor(
         statusCode,
         message = "Something went wrong",
         errors = [],
         stack = ""
-    ){
+    ) {
+
         super(message);
         this.statusCode = statusCode;
         this.message = message;
         this.success = false;
         this.errors = errors;
 
-        if(stack){
+        if (stack) {
             this.stack = stack
-        }else{
-            Error.captureStackTrace(this,this.constructor)
+        } else {
+            Error.captureStackTrace(this, this.constructor)
         }
     }
 }
 
-export {ApiError}
+export { ApiError }
