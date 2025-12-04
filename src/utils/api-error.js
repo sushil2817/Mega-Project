a35a452c5b7567b14ad34e8ef1e62aa2bd8c8dc1
sushil2817 <tony281723@gmail.com>
2025-12-04 22:45:16 +0530

    feat: add ApiResponse class and constants for user roles and task statuses

--- src/utils/api-error.js
+++ src/utils/api-error.js
@@ -1,23 +1,22 @@
-class ApiError extends Error {
+class ApiError extends Error{
     constructor(
         statusCode,
         message = "Something went wrong",
         errors = [],
         stack = ""
-    ) {
-
+    ){
         super(message);
         this.statusCode = statusCode;
         this.message = message;
         this.success = false;
         this.errors = errors;
-
-        if (stack) {
+        if(stack){
             this.stack = stack
-        } else {
-            Error.captureStackTrace(this, this.constructor)
+        }else{
+            Error.captureStackTrace(this,this.constructor)
         }
     }
 }
 
-export { ApiError }
+
+export {ApiError}
