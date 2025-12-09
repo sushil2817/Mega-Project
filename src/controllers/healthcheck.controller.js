6c06994b9981c20bd6f47a7cc4c53d2739793d8c
sushil2817 <tony281723@gmail.com>
2025-12-09 22:19:32 +0530

    feat: implement health check endpoint and add route/controller files for user, note, project, and task

---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,8 @@
+import {ApiResponse} from "../utils/api-response"
+const healthCheck = (req,res)=>{
+    res.status(200).json(
+        new ApiResponse(200,{message:"Server is running"})
+    );
+}
+
+export {healthCheck}
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,7 @@
 import mongoose, {Schema} from "mongoose";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken"
+import crypto from "crypto"
 
 const userSchema = new Schema(
     {
@@ -72,5 +74,38 @@ userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password,this.password)
 }
 
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn:process.env.ACCESS_TOKEN_EXPIRY}
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn:process.env.REFRESH_TOKEN_EXPIRY}
+    )
+}
+
+userSchema.methods.generateTemporaryToken = function(){
+    const unHashedToken = crypto.randomBytes(20).toString("hex")
+    const  hashedToken = crypto.createHash("sha256").update(unHashedToken).digest("hex")
+
+    const tokenExpiry = Date.now()+(20*60*1000)
+
+    return {
+        hashedToken,
+        unHashedToken,
+        tokenExpiry
+    }
+}
 
 export const User = mongoose.model("User",userSchema)
