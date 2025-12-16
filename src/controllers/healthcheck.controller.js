4306990922c731da49b8956ea5e3ff6ed45f4bc6
sushil2817 <tony281723@gmail.com>
2025-12-16 21:39:03 +0530

    db connection

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,4 +1,4 @@
-import {ApiResponse} from "../utils/api-response"
+import {ApiResponse} from "../utils/api-response.js"
 const healthCheck = (req,res)=>{
     res.status(200).json(
         new ApiResponse(200,{message:"Server is running"})
