5bb8314d9e2548d1a37da6fe6412a226696f6922
sushil2817 <tony281723@gmail.com>
2025-12-23 16:20:17 +0530

    changes

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -1,14 +1,14 @@
-import {validationResult} from "express-validator"
-import {ApiError} from "../utils/api-error.js"
-export const validate = (req,res,next) =>{
+import { validationResult } from "express-validator"
+import { ApiError } from "../utils/api-error.js"
+export const validate = (req, res, next) => {
     const errors = validationResult(req)
-    if(errors.isEmpty()){
+    if (errors.isEmpty()) {
         return next()
     }
     console.log(errors);
     const extractedError = []
-    errors.array().map((err)=>extractedError.push({
-        [err.path]:err.msg
+    errors.array().map((err) => extractedError.push({
+        [err.path]: err.msg
     }))
-    throw new ApiError(422,"Received data is not valid",extractedError);
+    throw new ApiError(422, "Received data is not valid", extractedError);
 }
