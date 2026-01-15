22ecf96a68b5dfe6b229d2a42ba087305db2a909
sushil2817 <tony281723@gmail.com>
2026-01-15 23:07:08 +0530

    refactor: enhance note controller and routes for improved error handling and validation

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -1,11 +1,97 @@
+import {Project} from "../models/project.model.js"
+import {ApiError} from "../utils/api-error.js"
+import {ProjectNote} from "../models/note.model.js"
+import {ApiResponse} from "../utils/api-response.js"
+import mongoose from "mongoose"
+
+
 const getNotes = async (req,res)=>{
     // get all notes
+    const {projectId} = req.params;
+
+    const project = await Project.findById(projectId) 
+    
+    if(!project){
+        throw new ApiError(404,"Proejct not Found")
+    }
+
+    const notes = await ProjectNote.find({
+        project: new mongoose.Types.ObjectId(projectId)
+    }).populate("createdBy","username fullName avatar")
+
+    return res.status(200).json(new ApiResponse(200,notes,"Notes fetch successfull"))
+
 }
 
 const getNoteById = async (req,res)=>{
     // get note by id
+    const {noteId} = req.params;
+
+    const note = await ProjectNote.findById(noteId).populate("createdBy","username fullname avatar")
+    if(!note){
+        throw new ApiError(404,"Note not found")
+    }
+    return res.status(200).json(new ApiResponse(200,note,"Note fetch successfully"));
 }
 
 const createNote = async (req,res)=>{
     // create note
+    const {projectId} = req.params;
+    const {content} = req.body;
+    const project = await Project.findById(projectId)
+    if(!project){
+        throw new ApiError(404,"Proejct not Found")
+    }
+    const note = ProjectNote.create({
+        project:new mongoose.Types.ObjectId(projectId),
+        content,
+        createdBy: new mongoose.Types.ObjectId(req.user._id)
+    })
+    const populatedNote =  await ProjectNote.findById(note._id).populate(
+        "createBy",
+        "username fullName avatar"
+    )
+    return res.status(200).json(new ApiResponse(200,populatedNote,"Note created successfully"));
+
+}
+
+const updateNote = async(req,res) =>{
+    // update note
+    const {noteId} = req.params;
+    const {content} = req.body;
+
+    const existingNote =  ProjectNote.findById(noteId);
+    if(!existingNote){
+        throw new ApiError(404,"Note not found")
+    }
+
+    const note = ProjectNote.findByIdAndUpdate(
+        noteId,
+        {content},
+        {new:true}
+    ).populate("createdBy", "username fullName avatar")
+
+    return res.status(200).json(new ApiResponse(200,note,"Note update successfully"));
+}
+
+const deleteNote = async (req,res) =>{
+    // delete note
+    const {noteID} = req.params;
+
+    const note = ProjectNote.findByIdAndDelete(noteID);
+
+    if(!note){
+        throw new ApiError(404,"Proejct not Found")
+    }
+
+    return res.status(200).json(new ApiResponse(200,note,"Note deleted successfully"));
+}
+
+export
+{
+    getNotes,
+    getNoteById,
+    createNote,
+    updateNote,
+    deleteNote
 }
--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -1,19 +1,18 @@
 import { Router } from "express";
-import {UserRoleEnum} from '../utils/constants.js'
+import {AvailableUserRoles, UserRoleEnum} from '../utils/constants.js'
 import { valideProjectPermission } from "../middlewares/auth.middleware.js";
+import {getNotes,createNote, getNoteById, updateNote, deleteNote} from "../controllers/note.controller.js"
 
 const router = Router();
 
 router.route("/:projectId")
-    .get(
-        valideProjectPermission(
-            [UserRoleEnum.ADMIN,UserRoleEnum.MEMBER]),
-        getNotes)
-    .post(
-        valideProjectPermission(
-            [UserRoleEnum.ADMIN], UserRoleEnum.MEMBER),
-        createNote
-    )
+    .get(valideProjectPermission(AvailableUserRoles),getNotes)
+    .post(valideProjectPermission([UserRoleEnum.ADMIN]),createNote)
 
+router
+.route(":/projectId/n/:noteId")
+.get(valideProjectPermission(AvailableUserRoles),getNoteById)
+.put(valideProjectPermission([UserRoleEnum.ADMIN]),updateNote)
+.delete(valideProjectPermission([UserRoleEnum.ADMIN]),deleteNote)
 
 export default router
