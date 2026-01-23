b80866283c8440c1fb04e84709f73a036a6b7ddd
sushil2817 <tony281723@gmail.com>
2026-01-23 16:25:13 +0530

    changes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,9 +3,7 @@ import { registerUser } from "../controllers/auth.controller.js";
 import {validate} from "../middlewares/validator.middleware.js"
 import {userRegistrationValidator} from "../validators/index.js"
 
-
 const router = Router();
 router.route("/register").post(userRegistrationValidator(),validate, registerUser);
-
 
 export default router
