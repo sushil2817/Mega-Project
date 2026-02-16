7018f8becc0998d0abb81cac7c5cec356863ff5a
sushil2817 <tony281723@gmail.com>
2026-02-16 11:04:17 +0530

    modify in routes folder

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 import {AvailableUserRoles, UserRoleEnum} from '../utils/constants.js'
 import { valideProjectPermission } from "../middlewares/auth.middleware.js";
 import {getNotes,createNote, getNoteById, updateNote, deleteNote} from "../controllers/note.controller.js"
+
 const router = Router();
 
 router.route("/:projectId")
     .get(valideProjectPermission(AvailableUserRoles),getNotes)
     .post(valideProjectPermission([UserRoleEnum.ADMIN]),createNote)
     
+
 router
 .route(":/projectId/n/:noteId")
 .get(valideProjectPermission(AvailableUserRoles),getNoteById)
