209022bdc7d37e9aa50ae2b7d3685cf91e920362
sushil2817 <tony281723@gmail.com>
2026-02-17 17:05:04 +0530

    Add new method in health check route

--- src/routes/healthcheck.routes.js
+++ src/routes/healthcheck.routes.js
@@ -3,5 +3,6 @@ import { healthCheck } from "../controllers/healthcheck.controller.js";
 const router = Router();
 
 // router.route("/").get(healthCheck)
+
 router.get("/",healthCheck)
 export default router
