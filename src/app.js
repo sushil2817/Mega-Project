30388b6268f014b6f88b18a0094a1c518ad04e22
sushil2817 <tony281723@gmail.com>
2026-08-27 07:49:13 +0530

    changes in app

--- src/app.js
+++ src/app.js
@@ -3,11 +3,16 @@ import express from "express"
 
 const app = express();
 
+app.use(express.json()); 
+
+app.use(express.urlencoded({ extended: true })); 
 
 // router imports
 import  healthCheckRouter  from "./routes/healthcheck.routes.js";
+import  registerUser from "./routes/auth.routes.js"
 
 app.use("/api/v1/healthcheck",healthCheckRouter)
+app.use("/api/v1",registerUser)
 
 
 export default app;
