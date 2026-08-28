5494f2d3fdd32d93dd8b731b76bd43b77fd59e12
sushil2817 <tony281723@gmail.com>
2026-08-28 18:49:34 +0530

    changes in auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,14 +1,46 @@
-
+import {ApiError} from  "../utils/api-error.js"
 import { asyncHandler } from "../utils/async-handler.js"
+import { User } from "../models/user.models.js"
+import { ApiResponse } from "../utils/api-response.js"
+import { body } from "express-validator"
 
-const registerUser = asyncHandler(async (req,res)=>{
-    const {email,username,password,role} = req.body
-    // validation
-    if(!email || !password){
+const registerUser = asyncHandler(async (req, res) => {
+    console.log("inside method");
+    console.log(req.body);
 
+    const { email, username, password, role, fullname } = req.body;
+
+    if (!email || !password) {
+        throw new ApiError(400, "Fields are missing");
     }
-    if(password.length < 8 ){}
-})
+
+    if (password.length < 8) {
+        throw new ApiError(400, "Password must be at least 8 characters");
+    }
+
+    const oldUser = await User.findOne({
+        email: email.toLowerCase()
+    });
+
+    if (oldUser) {
+        throw new ApiError(409, "User already exists");
+    }
+
+    const newUser = await new User({
+        email: email.toLowerCase(),
+        username,
+        password,
+        fullname,
+        role
+    }).save();
+
+    return res.status(201).json(
+        new ApiResponse(201, {
+            message: "User Added successfully!"
+        })
+    );
+});
+
 
 export {
     registerUser,
