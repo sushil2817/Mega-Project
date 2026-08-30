eacb174e2e1f2f117c1b0c4c9b7e10295e18d38d
sushil2817 <tony281723@gmail.com>
2026-08-30 22:50:15 +0530

    created auth route

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,15 @@
 import { Router } from "express";
 
 import { registerUser } from "../controllers/auth.controller.js" 
-import {validate} from "../minddlewares/validator.middlewares.js"
-import {userRegistrationValidator} from "../validators/index.js"
+// import {validate} from "../minddlewares/validator.middlewares.js"
+// import {userRegistrationValidator} from "../validators/index.js"
 
 const router = Router();
+// userRegistrationValidator(), 
+// validate, 
 
 router.route("/register").post(
-        userRegistrationValidator(), 
-        validate, 
         registerUser
     );
+
+export default router;
